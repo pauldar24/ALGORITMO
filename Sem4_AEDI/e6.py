4cb68414b992ac59bfebd1350b4e140978a84611
pauldar24 <pauldavid.anampa@gmail.com>
2025-08-22 16:25:33 -0500

    Create e6.py

---
+++ Sem4_AEDI/e6.py
@@ -0,0 +1,12 @@
+pilaFia = []
+
+pilaFia.append('A')
+pilaFia.append('B')
+pilaFia.append('C')
+
+print("Stack: ", pilaFia.stack)
+print("Pop: ", pilaFia.pop())
+print("Stack after Pop: ", pilaFia.stack)
+print("Peek: ", pilaFia.peek())
+print("isEmpty: ", pilaFia.isEmpty())
+print("Size: ", pilaFia.size())
